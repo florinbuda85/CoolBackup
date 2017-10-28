d314f5b1a7f1e9d245309fef6dd97b49e53c632a
florinbuda85 <florinbuda85@gmail.com>
2017-10-29 00:06:05 +0300

    Add MahApps Add Plan file

---
+++ CoolBackup/SettingsSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolBackup
+{
+    public class SettingsSingleton
+    {
+
+        private SettingsSingleton() { }
+
+        private static SettingsSingleton _instance = null;
+
+        public static SettingsSingleton getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SettingsSingleton();
+            }
+            return _instance;
+        }
+
+        /****/
+
+    }
+
+}
